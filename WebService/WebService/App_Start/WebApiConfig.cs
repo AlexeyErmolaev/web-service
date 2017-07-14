1ec2ff12982bde918b26b1bed5401be07e65ae0b
AlexeyErmolaev <79196495060@yandex.ru>
2017-07-14 15:56:21 +0300

    Fixed bugs

--- WebService/WebService/App_Start/WebApiConfig.cs
+++ WebService/WebService/App_Start/WebApiConfig.cs
@@ -8,10 +8,7 @@ namespace WebService
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
-
-            // Web API routes
+        { 
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
--- WebService/WebService/ComputeService.cs
+++ WebService/WebService/ComputeService.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
 
 namespace WebService
 {
     public static class ComputeService
     {
-        public static string Calculate(string expression)
+        public static double Calculate(string expression)
         {
+
             Stack<double> vals = new Stack<double>();
             Stack<char> operators = new Stack<char>();
             Stack<char> operatorsInBrackets = new Stack<char>();
@@ -17,19 +21,52 @@ namespace WebService
             bool inBrackets = false;
             int operatorsInBracketsCount = 0;
             int operatorsCount = 0;
+            bool firstValNegative = false;
+            bool firstValInBracketsNegative = false;
+
 
 
             for (int i = 0; i < input.Length; i++)
             {
                 var current = input[i];
 
+             
                 switch (current)
                 {
                     case '(':
                         inBrackets = true;
                         break;
-                    case '+':
                     case '-':
+                        if (inBrackets)
+                        {
+                            if (valsInBrackets.Count == 0)
+                            {
+                                firstValInBracketsNegative = true;
+
+                            }
+                            else
+                            {
+                                operatorsInBracketsCount++;
+                                operatorsInBrackets.Push(current);
+                            }
+                        }
+                        else
+                        {
+                            if (vals.Count == 0)
+                            {
+                                firstValNegative = true;
+
+                            }
+                            else
+                            {
+                                operatorsCount++;
+                                operators.Push(current);
+                            }
+                        }
+                        
+                        
+                        break;
+                    case '+':
                         if (inBrackets)
                         {
                             operatorsInBracketsCount++;
@@ -59,11 +96,11 @@ namespace WebService
                     case ')':
                         if(!inBrackets)
                         {
-                            return ("Expected '(' ");
+                            throw new FormatException($"Unexpected symbol: {current}");
                         }
                         for (int r = 0; r < operatorsInBracketsCount; r++)
                         {
-                            GetCalc(operatorsInBrackets, valsInBrackets);
+                                GetCalc(operatorsInBrackets, valsInBrackets);
                         }
                         multiplicationInBrackets = false;
                         inBrackets = false;
@@ -87,38 +124,56 @@ namespace WebService
                         case '7':
                         case '8':
                         case '9':
-                        if (inBrackets)
-                        {
+                            if (inBrackets)
+                            {
                             valsInBrackets.Push((int)char.GetNumericValue(current));
-                            if (multiplicationInBrackets)
+                            if (firstValInBracketsNegative)
                             {
-                                GetCalc(operatorsInBrackets, valsInBrackets);
-                                operatorsInBracketsCount--;
-                                multiplicationInBrackets = false;
+                                var val = valsInBrackets.Pop()*-1;
+                                valsInBrackets.Push(val);
+                                firstValInBracketsNegative = false;
                             }
-                        }
-                        else
-                        {
-                            vals.Push((int)char.GetNumericValue(current));
-                            if (multiplication)
+                            else
                             {
-                                if (operators.Count == 1 && vals.Count == 1)
+                                if (multiplicationInBrackets)
                                 {
-                                    return ("Incorrectly entered expression");
-                                }
-                                else
-                                {
-                                    GetCalc(operators, vals);
-                                    operatorsCount--;
-                                    multiplication = false;
+                                    GetCalc(operatorsInBrackets, valsInBrackets);
+                                    operatorsInBracketsCount--;
+                                    multiplicationInBrackets = false;
                                 }
                             }
-                        }
+                            }
+                            else
+                            {
+                                vals.Push((int)char.GetNumericValue(current));
+                                if (multiplication)
+                                {
+                                    if (operators.Count == 1 && vals.Count == 1)
+                                    {
+                                        throw new FormatException("Incorrectly entered expression");
+                                    }
+                                    else
+                                    {
+                                        if (firstValNegative)
+                                        {
+                                            GetCalcs(operators, vals);
+                                            firstValNegative = false;
+                                        }
+                                        else
+                                        {
+                                            GetCalc(operators, vals);
+                                        }
+                                        operatorsCount--;
+                                        multiplication = false;
+                                    }
+                                }
+                            }
+                        
                         break;
 
-                    default:
-                        return ("Incorrectly entered expression");
-                        break;
+                        default:
+                            throw new FormatException("Incorrectly entered expression");
+                            break;
                 }
             }
 
@@ -126,24 +181,61 @@ namespace WebService
             {
                 if (operators.Count == 1 && vals.Count == 1)
                 {
-                    return (Convert.ToString(operators.Pop()) + Convert.ToString(vals.Pop()));
+                    var op = operators.Pop();
+                    if(op == '-')
+                    {
+                        return vals.Pop() * -1;
+                    }
                 }
                 else
                 {
-                    GetCalc(operators, vals);
+                    if (firstValNegative)
+                    {
+                        GetCalcs(operators, vals);
+                        firstValNegative = false;
+                    }
+                    else
+                    {
+                        GetCalc(operators, vals);
+                    }
                     operatorsCount = 0;
                 }
             }
             if (inBrackets)
             {
-                return ("Expected')'");
+                throw new FormatException("Missing ')'");
             }
             else
             {
-                return Convert.ToString(vals.Pop());
+                return vals.Pop();
             }
         }
+        private static void GetCalcs(Stack<char> operators, Stack<double> vals)
+        {
+            char op = operators.Pop();
+            double lastValue = vals.Pop();
+            double res = vals.Pop()*-1;
 
+            switch (op)
+            {
+                case '+':
+                    vals.Push(lastValue + res);
+                    break;
+                case '-':
+                    vals.Push(res - lastValue);
+                    break;
+                case '*':
+                    vals.Push(lastValue * res);
+                    break;
+                case '/':
+                    vals.Push(res / lastValue);
+                    break;
+                default:
+                    throw new NotImplementedException("Unexpected operator's value");
+            }
+            lastValue = 0;
+            res = 0;
+        }
         private static void GetCalc(Stack<char> operators, Stack<double> vals)
         {
             char op = operators.Pop();
